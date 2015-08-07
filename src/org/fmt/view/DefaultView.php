408fd1e1addb1774c17d6cac2a6483b7d7df7929
Yamil Juri <yamil.juri@sitrack.com>
2015-08-07 16:29:10 -0300

    Correccion

--- src/org/fmt/view/DefaultView.php
+++ src/org/fmt/view/DefaultView.php
@@ -4,9 +4,10 @@ namespace org\fmt\view;
 
 use NeoPHP\web\html\HTMLView;
 use NeoPHP\web\html\Tag;
+use org\fmt\view\component\Navegador;
 
 abstract class DefaultView extends HTMLView
-{
+{    
     protected function build()
     {
         parent::build();
@@ -16,18 +17,16 @@ abstract class DefaultView extends HTMLView
         $this->addMeta(array("name"=>"viewport", "content"=>"width=device-width, initial-scale=1.0"));
         $this->addScriptFile($this->getBaseUrl() . "res/assets/jquery-1.11.2/jquery.min.js");
         $this->addScriptFile($this->getBaseUrl() . "res/assets/bootstrap-3.3.4/js/bootstrap.min.js");
+        $this->addScriptFile($this->getBaseUrl() . "res/sb/js/plugins/morris/raphael.min.js");
+        $this->addScriptFile($this->getBaseUrl() . "res/sb/js/plugins/morris/morris.min.js");
+        $this->addScriptFile($this->getBaseUrl() . "res/sb/js/plugins/morris/morris-data.js");
         $this->addStyleFile($this->getBaseUrl() . "res/sb/css/bootstrap.min.css");
         $this->addStyleFile($this->getBaseUrl() . "res/sb/css/sb-admin.css");
         $this->addStyleFile($this->getBaseUrl() . "res/sb/css/plugins/morris.css");
         $this->addStyleFile($this->getBaseUrl() . "res/sb/font-awesome/css/font-awesome.min.css");
         
-        
-                
         $this->getBodyTag()->add($this->createMainHeader());
         $this->getBodyTag()->add($this->createMainContent());
-        
-        
-        
         
     }
     
@@ -58,23 +57,38 @@ abstract class DefaultView extends HTMLView
 //               </div>';
         
         $tag = new Tag("div",array('id'=>'wrapper'));
-        $tag->add($this->createHeaderContent());
+        $nav = new Navegador(['class'=>'navbar navbar-inverse navbar-fixed-top', 'role'=>'navigation']);
+        $nav->addMenu($this->createTitle ());
+        $nav->addMenu($this->createHeaderContent());
+        $tag->add($nav);
+        $tag->add($this->createContent());
         return $tag;
         
     }
     
     protected function createMainContent ()
     {
-        return '
-        <div id="mainContent">
-            <div id="mainBody">
-                ' . $this->createContent() . '
-            </div>
-        </div>';
+//        return '
+//        <div id="mainContent">
+//            <div id="mainBody">
+//                ' . $this->createContent() . '
+//            </div>
+//        </div>';
+//        
+        
     }
     
+    protected function createTitle(){
+        
+        $div = new Tag("div",['class'=>'navbar-header']);   
+        $div->add(new Tag("a",['class'=>"navbar-brand", 'href'=>'index.html'],  $this->setHeaderTitle()));
+        return $div;
+    }
+
+
     protected abstract function createHeaderContent();
     protected abstract function createContent();
+    protected abstract function setHeaderTitle();
 }
 
 ?>
