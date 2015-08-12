b0b6be55006a501f315b14c9b8e893d81e04a36b
Yamil Juri <yamil.juri@sitrack.com>
2015-08-12 16:46:55 -0300

    form

--- src/org/fmt/view/component/Form.php
+++ src/org/fmt/view/component/Form.php
@@ -26,6 +26,28 @@ class Form extends Tag
         }
     }
     
+    public function addElement($tag,$name,$title){
+        
+        $component= null;
+        
+        switch ($name){
+            
+            case "text":
+                $div = new Tag('div',['class'=>'form-group']);
+                $div->add(new Tag('label',['For'=>'email']));
+                $div->add(new Tag('input',['type'=>"$tag",'class'=>'form-control','id'=>"$name",'name'=>"$name"],$title));
+                
+//                <div class="form-group">
+//			< f>Email</label>
+//			<input type="email" class="form-control" id="email" placeholder="Tu email" name="email">
+//			</div>
+                $component = $div;
+                break;
+            
+        }
+        
+        return $component;
+    }
 }
 
 ?>
